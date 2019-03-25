4f72058ef6d9b83a81e637c1cc667a67d43232a4
Haydar Ciftci <haydar.ciftci@nobelbiocare.com>
2019-03-25 14:34:31 +0100

    Add exec flag to push script

--- docker_push.sh
+++ docker_push.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-set -o errexit
-#echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push memoryleak/php:7.1
 docker push memoryleak/php:7.1-fpm
 docker push memoryleak/php:7.1-fpm-xdebug
