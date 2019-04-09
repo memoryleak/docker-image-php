82e44f5506e0a513fe277c4432c193249bcdbc75
Haydar Ciftci <hciftci@localhost.localdomain>
2019-04-09 22:55:55 +0200

    Add 7.1-xdebug to push script

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,16 @@ docker push memoryleak/php:7.1-$TRAVIS_BUILD_NUMBER
 docker push memoryleak/php:7.1-fpm-$TRAVIS_BUILD_NUMBER
 docker push memoryleak/php:7.1-fpm-xdebug-$TRAVIS_BUILD_NUMBER
 docker push memoryleak/php:7.1-composer-$TRAVIS_BUILD_NUMBER
+docker push memoryleak/php:7.1-xdebug-$TRAVIS_BUILD_NUMBER
 
 docker tag memoryleak/php:7.1-$TRAVIS_BUILD_NUMBER memoryleak/php:7.1
 docker tag memoryleak/php:7.1-fpm-$TRAVIS_BUILD_NUMBER memoryleak/php:7.1-fpm
 docker tag memoryleak/php:7.1-fpm-xdebug-$TRAVIS_BUILD_NUMBER memoryleak/php:7.1-fpm-xdebug
 docker tag memoryleak/php:7.1-composer-$TRAVIS_BUILD_NUMBER memoryleak/php:7.1-composer
+docker tag memoryleak/php:7.1-xdebug-$TRAVIS_BUILD_NUMBER memoryleak/php:7.1-xdebug
 
 docker push memoryleak/php:7.1-$TRAVIS_BUILD_NUMBER
 docker push memoryleak/php:7.1-fpm-$TRAVIS_BUILD_NUMBER
 docker push memoryleak/php:7.1-fpm-xdebug-$TRAVIS_BUILD_NUMBER
 docker push memoryleak/php:7.1-composer-$TRAVIS_BUILD_NUMBER
+docker push memoryleak/php:7.1-xdebug-$TRAVIS_BUILD_NUMBER
